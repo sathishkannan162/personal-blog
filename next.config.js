ce436ed951354d1133f959ed45f98883ed93a432
Sathish Kannan <sathishkannan162@gmail.com>
2023-12-16 02:51:59 +0530

    Add basic mdx support and config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// eslint-disable-next-line import/no-extraneous-dependencies , @typescript-eslint/no-var-requires
+const withMDX = require('@next/mdx')();
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
+
+module.exports = withMDX(nextConfig);
