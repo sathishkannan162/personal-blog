023e58d7cf9f073d805b9eed8d5e4bcc50a4d5bb
Sathish Kannan <sathishkannan162@gmail.com>
2023-12-16 02:50:35 +0530

    Fix mdx error for images

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,14 +5,13 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <h1 className="text-[50px]">{children}</h1>,
     img: (props) => (
+      // @ts-expect-error src incompatibly typed
       // eslint-disable-next-line jsx-a11y/alt-text
       <Image
-        // sizes="100vw"
-        // style={{ width: '100%', height: 'auto' }}
-        // @ts-expect-error string number type mismatch
-        width={100}
-        // @ts-expect-error string number type mismatch
-        height={100}
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+        // width={100}
+        // height={100}
         {...props}
       />
     ),
